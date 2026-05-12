65aa180b1b4576069090b7da6800df93509a2f92
Praveen Ambati <praveen.ambati@nttdata.com>
2026-05-12 10:18:24 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello from Node.js backend!"
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
