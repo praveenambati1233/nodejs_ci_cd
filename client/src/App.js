c231c7ff5c4e0ff4e7178b83e7e7f226e7694f2a
Praveen Ambati <praveen.ambati@nttdata.com>
2026-05-12 10:41:53 +0530

    add new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
-import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  const callBackend = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/api/hello");
-      const data = await response.json();
-
-      setMessage(data.message);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>React + Node.js POC</h1>
-
-      <button onClick={callBackend}>
-        Call Backend
-      </button>
-
-      <p>{message}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
